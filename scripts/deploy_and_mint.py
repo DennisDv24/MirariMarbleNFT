448bd7b03d70dc37a1a5648415651ba0c351adac
DennisDv24 <jokarboxing@gmail.com>
2022-08-03 10:02:09 +0200

    deployment done

--- scripts/deploy_and_mint.py
+++ scripts/deploy_and_mint.py
@@ -6,7 +6,9 @@ MIRARI_ADDR = '0xbb884cfc40c40a7bafc12f54d4d511406f3969cf'
 
 LOCAL_ENVS = ['development', 'ganache']
 
-META_BASE_URI = 'ar://8avbVHlVIp5iS2pbCvDFhIZiSRPJnA4rt0YWINU0Jvo/'
+META_BASE_URI = 'https://arweave.net/8avbVHlVIp5iS2pbCvDFhIZiSRPJnA4rt0YWINU0Jvo/'
+
+
 META_BASE_NAME = 'MirariMarbleOG0'
 ITERATIONS = 10
 
@@ -21,18 +23,21 @@ should_verify = config['networks'][network.show_active()].get('verify', False)
 deploy_token = lambda: MiraiMarble.deploy(from_me, publish_source = should_verify)
 
 def mint_tokens_with(mint_function):
+    input('Continue?')
     for i in range(ITERATIONS):
+        input('Continue?')
         mint_function(
             MIRARI_ADDR,
             f'{META_BASE_URI}{META_BASE_NAME}{i}.json',
             from_me
-        ).wait(1)
+        )
     
 
 
 def deploy():
     mint_tokens_with(deploy_token().mintNewMarble)
-    MiraiMarble[-1].transferOwnership(MIRARI_ADDR, from_me)
+    input('All minted, transfer ownerhisp?')
+    MiraiMarble[-1].transferOwnership(MIRARI_ADDR, from_me).wait(1)
 
 def main():
     deploy()
--- scripts/gas_estimator.py
+++ scripts/gas_estimator.py
@@ -1,20 +1,28 @@
 from brownie import MiraiMarble
 from brownie import accounts, network, config
-from scripts.deploy_and_mint import get_acc, from_me, deploy_token
+from scripts.deploy_and_mint import get_acc, from_me, deploy_token, MIRARI_ADDR
 from web3 import Web3
 
+EXAMPLE_URI = 'https://arweave.net/8avbVHlVIp5iS2pbCvDFhIZiSRPJnA4rt0YWINU0Jvo/MirariMarbleOG07.json'
+
 GAS_PRICE = Web3.toWei(30, 'gwei')
+gas_to_eth = lambda x: Web3.fromWei(x * GAS_PRICE, 'ether')
+
 
 def estimate_gas():
-    gas = MiraiMarble.deploy.estimate_gas(from_me)
-    eth_price = Web3.fromWei(gas * GAS_PRICE, 'ether')
-    print(f'Gas cost: {gas}')
-    print(f'Current apx price for this contract: {eth_price}')
+    deploy_gas = MiraiMarble.deploy.estimate_gas(from_me)
+    print(f'Deploy gas cost: {deploy_gas}')
+    print(f'Current apx deployment price: {gas_to_eth(deploy_gas)} eth')
     token = deploy_token()
-    gas = token.mintNewMarble.estimate_gas(get_acc(), '', from_me)
-    eth_price = Web3.fromWei(gas * GAS_PRICE, 'ether')
-    print(f'Gas cost: {gas}')
-    print(f'Current apx price for this contract: {eth_price}')
+    mint_gas = token.mintNewMarble.estimate_gas(MIRARI_ADDR, EXAMPLE_URI, from_me)
+    print(f'Mint gas cost: {mint_gas}')
+    print(f'Current apx minting price: {gas_to_eth(mint_gas)} eth')
+    print(f'10 mints price: {gas_to_eth(mint_gas) * 10} eth')
+    ownership_gas = token.transferOwnership.estimate_gas(MIRARI_ADDR, from_me)
+    print(f'Renouncement gas cost: {ownership_gas}')
+    print(f'Current apx renouncement price: {gas_to_eth(ownership_gas)} eth')
+    print(f'\nTotal estimation: {gas_to_eth(mint_gas * 10 + ownership_gas + deploy_gas)} eth')
+
     
 
 def main():
