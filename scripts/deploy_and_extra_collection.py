16a72c275b72fbd8717e422e4e9f7170d46fe9c3
DennisDv24 <jokarboxing@gmail.com>
2022-08-12 21:27:44 +0200

    new metadata and opensea testing

--- scripts/deploy_and_extra_collection.py
+++ scripts/deploy_and_extra_collection.py
@@ -3,28 +3,45 @@ from brownie import network, config, accounts
 from scripts.deploy_factory import DEPLOYER_TEST_ADDR, CLIENT_TEST_ADDR
 from scripts.deploy_factory import deploy_to_testnet, get_acc
 
-EXAMPLE_URI = ''
+META_ARWEAVE_MANIFEST_HASH = 'vYYPN3MKBa7oojy7chE0AcIv0IkNJQ2hvlqLHf_aHwk'
+META_BASE_URI = f'https://arweave.net/{META_ARWEAVE_MANIFEST_HASH}/'
+META_BASE_NAME = 'MMHonorary'
+META_EXT = '.json'
+TOTAL_TOKENS = 12
+
+format_uri_id = lambda i: i if len(str(i)) > 1 else f'0{i}'
+
+get_uri = lambda i: (
+    f'{META_BASE_URI}{META_BASE_NAME}{format_uri_id(i)}{META_EXT}'
+)
 
 def test_minting_extra_tokens(collection_addr):
     collection = MirariMarbleCollection.at(collection_addr)
     collection.transferOwnership(
         DEPLOYER_TEST_ADDR, {'from': CLIENT_TEST_ADDR}
     )
-    # NOTE in production it would be a loop that would
-    # deploy a lot of new marbles, this is only
-    # for testing if the opensea front-end ownership works 
-    # right after multiple ownership transfers
-    collection.mintNewMarble(CLIENT_TEST_ADDR, EXAMPLE_URI)
-    collection.transferOwnership(
+    for i in range(TOTAL_TOKENS):
+        collection.mintNewMarble(
+            CLIENT_TEST_ADDR, get_uri(i), {'from': DEPLOYER_TEST_ADDR}
+        )
+    tx = collection.transferOwnership(
         CLIENT_TEST_ADDR, {'from': DEPLOYER_TEST_ADDR}
     )
+    tx.wait(1)
+
+def deploy_to_testnet_full_test():
+    deploy_to_testnet()
+    colle_tx = MarbleFactory[-1].deployMainCollection({'from': CLIENT_TEST_ADDR})
+    colle_tx.wait(1)
+    new_colle = input('Introduce new collection addr: ')
+    test_minting_extra_tokens(new_colle)
 
 
-def mint_extra_mainnet():
+
+def mint_extra_tokens_in_mainnet():
     pass
 
 def main():
-    deploy_to_testnet()
-    colle = MarbleFactory[-1].deployMainCollection({'from': CLIENT_TEST_ADDR})
-    test_minting_extra_tokens(colle)
-    #deploy_to_production()
+    mint_extra_tokens_in_mainnet()
+
+
--- scripts/deploy_factory.py
+++ scripts/deploy_factory.py
@@ -13,12 +13,12 @@ def get_acc(wallet_name = None):
 MIRARI_ADDR = '0xbb884cfc40c40a7bafc12f54d4d511406f3969cf'
 
 should_verify = config['networks'][network.show_active()].get('verify', False)
-
+ask_for_verify = lambda x: input(f'Should verify {x}? y/N: ').lower() == 'y'
 
 def local_deploy():
     factory = MarbleFactory.deploy(
         {'from': get_acc('test1')},
-         publish_source=should_verify
+         publish_source=ask_for_verify('MarbleFactory')
     )
     input('Factory deployed, continue?')
     tx = factory.transferOwnership(get_acc(), {'from': get_acc('test1')})
@@ -32,7 +32,7 @@ CLIENT_TEST_ADDR = get_acc()
 def deploy_to_testnet():
     factory = MarbleFactory.deploy(
         {'from': DEPLOYER_TEST_ADDR},
-         publish_source=should_verify
+         publish_source=ask_for_verify('MarbleFactory')
     )
     input('Factory deployed, continue?')
     tx = factory.transferOwnership(CLIENT_TEST_ADDR, {'from': DEPLOYER_TEST_ADDR})
@@ -42,7 +42,7 @@ def deploy_to_testnet():
 def deploy_to_production():
     factory = MarbleFactory.deploy(
         {'from': get_acc()},
-         publish_source=should_verify
+         publish_source=ask_for_verify('MarbleFactory')
     )
     input('Factory deployed, continue?')
     tx = factory.transferOwnership(MIRARI_ADDR, {'from': get_acc()})
